bbed5519beef1b4253c1da8ca6675b635f1b8bc0
robot <lincolnmorais@gmail.com>
2019-11-14 23:50:50 +0100

    v1.04

--- selenium_chrome.py
+++ selenium_chrome.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python
 import os
 import sys
-import json
 from selenium import webdriver
+
+window_size = "1920x1080"
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36"
+headless = True
+enable_images = False
 
 # Defining some options
 options = webdriver.ChromeOptions()
-options.add_argument("--no-sandbox")
-options.add_argument('headless')
-options.add_argument("disable-infobars")
-options.add_argument('window-size=1920x1080')
+options.add_argument('--no-sandbox')
+options.add_argument('headless' if headless == True else '')
+options.add_argument('disable-infobars')
+options.add_argument('window-size='+window_size)
 options.add_argument('--disable-logging')
-options.add_argument("--lang=en-US")
-options.add_argument('--blink-settings=imagesEnabled=false')
-options.add_argument(
-	"--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36")
+options.add_argument('--lang=en-US')
+options.add_argument('--blink-settings=imagesEnabled=false' if enable_images == True else '')
+options.add_argument('--user-agent='+user_agent)
 
 # Verifying the SO and choosing the correctly bin
 platform = sys.platform
-if platform == "linux" or platform == "linux2":
+if platform == 'linux' or platform == 'linux2':
 	chrome = webdriver.Chrome(chrome_options=options,
-							  executable_path="bin/chromedriver_linux")
+							  executable_path='bin/chromedriver_linux')
 else:
 	chrome = webdriver.Chrome(chrome_options=options,
-							  executable_path="bin/chromedriver_mac")
+							  executable_path='bin/chromedriver_mac')
 
 # navigating to the url and printing the results
 url = 'https://duckduckgo.com/?q=houses&ia=web'
